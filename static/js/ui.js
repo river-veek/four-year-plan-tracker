f0be2071f38730a39db0b5327203c7c2888bb79e
Logan Levitre <loganlevitre@gmail.com>
2021-02-16 18:03:06 -0800

    fixed scaling/centering

--- static/js/ui.js
+++ static/js/ui.js
@@ -1,15 +1,14 @@
 function toggleShow(){
-	document.getElementById("dropdown-list").classList.toggle("show");
+  document.getElementById('dropdown-list').classList.toggle('show')
 }
 
-window.onclick = function(event){
-	if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    const dropdowns = document.getElementsByClassName('dropdown-content')
+    for (let i = 0; i < dropdowns.length; i++) {
+      const openDropdown = dropdowns[i]
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show')
       }
     }
   }
