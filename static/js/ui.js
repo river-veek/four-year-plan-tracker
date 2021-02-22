dd2fe10ae202b5a0d83011a8a3467ec024b33b3b
Logan Levitre <loganlevitre@gmail.com>
2021-02-22 12:02:22 -0800

    cleaned up js

--- static/js/ui.js
+++ static/js/ui.js
@@ -1,132 +1,121 @@
 
-function toggleShow() {
-  document.getElementById('dropdown-list').classList.toggle('show');
+function toggleShow () {
+  document.getElementById('dropdown-list').classList.toggle('show')
 }
 
-function toggleLogin() {
-	 var button = document.getElementById('log-in');
-	 var add = document.getElementById('add');
-	 var save = document.getElementById('save');
-	 var displaybtn = document.getElementById('display');
-	 var remove = document.getElementById('remove');
+function toggleLogin () {
+  const button = document.getElementById('log-in')
+  const add = document.getElementById('add')
+  const save = document.getElementById('save')
+  const displaybtn = document.getElementById('display')
+  const remove = document.getElementById('remove')
 
-	button.style.display = "none";
-    createLists()
-    add.style.display = "inline";
-    save.style.display = "inline";
-	remove.style.display ="inline";
-    displaybtn.style.display = "inline";
-	//getElementsByClassName('functions').style.display='inline';
+  button.style.display = 'none'
+  createLists()
+  add.style.display = 'inline'
+  save.style.display = 'inline'
+  remove.style.display = 'inline'
+  displaybtn.style.display = 'inline'
+  // getElementsByClassName('functions').style.display='inline';
 }
 
-function createLists(){
-	createCourseList()
-	createTermList()
-	createYearList()
-	//addTexts()
-	
+function createLists () {
+  createCourseList()
+  createTermList()
+  createYearList()
+  // addTexts()
 }
 
-
-//Takes current option value - adds it to table
-function addClass()
-{
-	//get the current option values selected
-	let currCourse = document.getElementById("choice-list");
-	let currTerm = document.getElementById("term-list");
-	let currYear = document.getElementById("year-list");
-	//get table
-	let table = document.getElementById('added-list');
-	//insert new row
-	table.insertRow();
-	//always add 3 cells as only inserting choice, term and year
-	for(let x = 0; x < 3; x++)
-	{	//insert new cell into row
-	    table.rows[table.rows.length - 1].insertCell();
-	}
-	let currRow = table.rows[table.rows.length - 1].cells
-	//Set each cell to correct value;
-	currRow[0].textContent = currCourse.value;
-	currRow[1].textContent = currYear.value;
-	currRow[2].textContent = currTerm.value;
+// Takes current option value - adds it to table
+function addClass () {
+  // get the current option values selected
+  const currCourse = document.getElementById('choice-list')
+  const currTerm = document.getElementById('term-list')
+  const currYear = document.getElementById('year-list')
+  // get table
+  const table = document.getElementById('added-list')
+  // insert new row
+  table.insertRow()
+  // always add 3 cells as only inserting choice, term and year
+  for (let x = 0; x < 3; x++) {
+  // insert new cell into row
+    table.rows[table.rows.length - 1].insertCell()
+  }
+  const currRow = table.rows[table.rows.length - 1].cells
+  // Set each cell to correct value;
+  currRow[0].textContent = currCourse.value
+  currRow[1].textContent = currYear.value
+  currRow[2].textContent = currTerm.value
 }
 
-function removeClass(){
-	//get the current option values selected
-	let currCourse = document.getElementById("choice-list").value;
-	let currTerm = document.getElementById("term-list").value;
-	let currYear = document.getElementById("year-list").value;
-	//get table
-	var currTable = document.getElementById('added-list');
-	let len = currTable.rows.length - 1
-	let found = false;
-	var idx = 1;
-	while(!found && idx <= len)
-	{
-		var currRow = currTable.rows[idx].cells
-		if(currRow[0].innerHTML == currCourse && currRow[1].innerHTML == currYear && currRow[2].innerHTML == currTerm)
-		{
-			found = true;
-			currTable.deleteRow(idx);
-		}
-		else
-		{
-			idx++;
-		}
-	}
-	
+function removeClass () {
+  // get the current option values selected
+  const currCourse = document.getElementById('choice-list').value
+  const currTerm = document.getElementById('term-list').value
+  const currYear = document.getElementById('year-list').value
+  // get table
+  const currTable = document.getElementById('added-list')
+  const len = currTable.rows.length - 1
+  let found = false
+  let idx = 1
+  while (!found && idx <= len) {
+    const currRow = currTable.rows[idx].cells
+    if (currRow[0].innerText === currCourse && currRow[1].innerText === currYear && currRow[2].innerText === currTerm) {
+      found = true
+      currTable.deleteRow(idx)
+    } else { idx++ }
+  }
+}
+// Builds course list for now - used for testing
+function createCourseList () {
+  const classesList = ['Courses', '210: Computer Science I', '211: Computer Science II', '212: Computer Science III', '313: Intermediate Data Structures', '314: Computer Organization',
+    '315: Intermediate Algorithms', '322: Introduction to Software Engineering', '330: C/C++ & Unix', '399: Applied Cryptography']
+  const list = document.createElement('select')
+  list.name = 'Courses'
+  list.id = 'choice-list'
+  list.class = 'c-list'
+  for (const val of classesList) {
+    const option = document.createElement('option')
+    option.value = val
+    option.text = val
+    list.appendChild(option)
+  }
+  document.getElementById('init').appendChild(list)
+}
+// Builds term list for now - used for testing
+function createTermList () {
+  const termList = ['Terms', 'Fall', 'Winter', 'Spring', 'Summer']
+  const tlist = document.createElement('select')
+  tlist.name = 'Term'
+  tlist.id = 'term-list'
+  tlist.class = 't-list'
 
+  for (const val of termList) {
+    const option = document.createElement('option')
+    option.value = val
+    option.text = val
+    tlist.appendChild(option)
+  }
+  document.getElementById('init').appendChild(tlist)
 }
+// Builds year list for now - used for testing
+function createYearList () {
+  const yearList = ['Year', '1st', '2nd', '3rd', '4th', '5th']
+  const ylist = document.createElement('select')
+  ylist.name = 'Year'
+  ylist.id = 'year-list'
+  ylist.class = 'y-list'
 
-//Builds course list for now - used for testing
-function createCourseList(){
-	var classesList = ["Courses", "210: Computer Science I","211: Computer Science II","212: Computer Science III","313: Intermediate Data Structures","314: Computer Organization",
-	"315: Intermediate Algorithms","322: Introduction to Software Engineering","330: C/C++ & Unix","399: Applied Cryptography"]
-	var list = document.createElement('select');
-	list.name = "Courses";
-	list.id="choice-list";
-	list.class="c-list";
-	for(const val of classesList){
-		var option = document.createElement("option");
-		option.value = val;
-		option.text = val;
-		list.appendChild(option);
-	}
-	document.getElementById('init').appendChild(list);
-}
-//Builds term list for now - used for testing
-function createTermList() {
-	var termList = ["Terms","Fall", "Winter", "Spring", "Summer"]
-	var tlist = document.createElement('select');
-	tlist.name = "Term";
-	tlist.id="term-list";
-	tlist.class="t-list";
-	for(const val of termList){
-		var option = document.createElement("option");
-		option.value = val;
-		option.text = val;
-		tlist.appendChild(option);
-	}
-	document.getElementById('init').appendChild(tlist);
-	
-}
-//Builds year list for now - used for testing
-function createYearList(){
-	var yearList = ["Year","1st","2nd","3rd","4th","5th"]
-	var ylist = document.createElement('select');
-	ylist.name = "Year";
-	ylist.id="year-list";
-	ylist.class="y-list";
-	for(const val of yearList){
-		var option = document.createElement("option");
-		option.value = val;
-		option.text = val;
-		ylist.appendChild(option);
-	}
-	document.getElementById('init').appendChild(ylist);
+  for (const val of yearList) {
+    const option = document.createElement('option')
+    option.value = val
+    option.text = val
+    ylist.appendChild(option)
+  }
+  document.getElementById('init').appendChild(ylist)
 }
 
-//toggles dropdown for log in
+// toggles dropdown for log in
 window.onclick = function (event) {
   if (!event.target.matches('.dropbtn')) {
     const dropdowns = document.getElementsByClassName('dropdown-content')
@@ -138,5 +127,3 @@ window.onclick = function (event) {
     }
   }
 }
-
-
