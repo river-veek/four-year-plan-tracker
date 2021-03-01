7ea1396d2182f9f502f145169ef9f13d174ca02b
River Veek <riverv@uoregon.edu>
2021-02-28 19:16:18 -0800

    Added dummy CIS degree object for testing purposes, beginning to add more nosetests

--- testing/test_degree_objects.py
+++ testing/test_degree_objects.py
@@ -1,1 +1,29 @@
-# test degree objects 
+"""
+--------------------------------------------------------------------------------
+File containing nose tests for degree_objects.py.
+
+Author: River Veek
+Group: TBD
+Last Modified: 2/28/21
+--------------------------------------------------------------------------------
+"""
+import nose
+import sys
+sys.path.append("../degree_logic")
+from degree_objects import *
+
+
+class Test_degree_object:
+
+    def test_add_course(self):
+        """Test adding single course to dummy degree object."""
+        deg = Degree("TEST")
+        term1 = Term("Fall")
+        deg.add_course("Course 1", 101, 4, [], [term1])
+        assert len(deg.courses) == 1
+        assert deg.courses[0].name == "Course 1"
+        assert deg.courses[0].num_credits == 4
+        assert len(deg.courses[0].pre_reqs) == 0
+        assert len(deg.courses[0].terms) == 1
+        assert deg.courses[0].terms == [term1]
+        assert deg.courses[0].course_num == 101
--- testing/test_generate_plan.py
+++ testing/test_generate_plan.py
@@ -44,6 +44,53 @@ def test_change_summer():
 def test_create_plan():
     pass
 
+def create_test_degree():
+    test_major = Degree("Test Major")
+
+    ################# Writing and Math Pre-Requisites ##########################
+    # --------------------------------------------------------------------------
+    test_major.add_course("WR 121 College Composition I", 121, 4, [], [Term("Fall"), Term("Winter"), Term("Spring"), Term("Summer")])
+    test_major.add_course("WR 122 College Composition II", 121, 4, ["WR 121 College Composition I"], [Term("Fall"), Term("Winter"), Term("Spring"), Term("Summer")])
+
+    # Math 112 added as a pre-req (alternatively this can be satisfied by an adequate placement test score)
+    test_major.add_course("MATH 112 Elementary Functions", 112, 4, [], [Term("Fall"), Term("Winter"), Term("Spring"), Term("Summer")], is_core = True)
+
+    # Discrete Math I & II (required of CIS majors)
+    test_major.add_course("MATH 231 Elements of Discrete Mathematics I", 231, 4, ["MATH 112 Elementary Functions"], [Term("Fall"), Term("Winter"), Term("Spring")], is_core = True)
+    test_major.add_course("MATH 232 Elements of Discrete Mathematics II", 232, 4, ["MATH 231 Elements of Discrete Mathematics I"], [Term("Fall"), Term("Winter"), Term("Spring")], is_core = True)
+    # --------------------------------------------------------------------------
+
+    ##################### 200 level test courses ########################
+    # --------------------------------------------------------------------------
+    test_major.add_course("CIS 210 Computer Science I", 210, 4, ["MATH 112 Elementary Functions"], [Term("Fall"), Term("Winter")], is_core = True)
+    test_major.add_course("CIS 211 Computer Science II", 211, 4, ["CIS 210 Computer Science I"], [Term("Winter"), Term("Spring")], is_core = True)
+    test_major.add_course("CIS 212 Computer Science III", 212, 4, ["CIS 211 Computer Science II"], [Term("Fall"), Term("Spring")], is_core = True)
+    # --------------------------------------------------------------------------
+
+    ######################## 300 level CIS courses #############################
+    # --------------------------------------------------------------------------
+    test_major.add_course("CIS 313 Intermediate Data Structures", 313, 4, ["CIS 212 Computer Science III", "MATH 232 Elements of Discrete Mathematics II"], [Term("Fall"), Term("Winter")], is_core = True)
+    test_major.add_course("CIS 314 Computer Organization", 314, 4, ["CIS 212 Computer Science III", "MATH 231 Elements of Discrete Mathematics I"], [Term("Winter"), Term("Spring")], is_core = True)
+    test_major.add_course("CIS 315 Intermediate Algorithms", 315, 4, ["CIS 313 Intermediate Data Structures"], [Term("Winter"), Term("Spring")], is_core = True)
+    test_major.add_course("CIS 322 Introduction to Software Engineering", 322, 4, ["CIS 212 Computer Science III"], [Term("Fall"), Term("Spring")])
+    test_major.add_course("CIS 330 C/C++ & Unix", 330, 4, ["CIS 314 Computer Organization"], [Term("Winter"), Term("Spring")], is_core = True)
+
+    ################# Upper Division (400 level) CIS courses ###################
+    # --------------------------------------------------------------------------
+    test_major.add_course("CIS 415 Operating Systems", 415, 4, ["CIS 313 Intermediate Data Structures", "CIS 330 C/C++ & Unix"], [Term("Fall"), Term("Spring")], is_core = True)
+    test_major.add_course("CIS 429 Computer Architecture", 431, 4, ["CIS 330 C/C++ & Unix"], [Term("Spring")])
+
+    ################# CIS Electives (400/300 level) ###################
+    # --------------------------------------------------------------------------
+    test_major.add_course("CIS Elective 400+", 400, 4, ["CIS 212 Computer Science III"], [Term("Fall"), Term("Winter"), Term("Spring"), Term("Summer")], is_core = True)
+    test_major.add_course("CIS Elective 300+", 300, 4, ["CIS 212 Computer Science III"], [Term("Fall"), Term("Winter"), Term("Spring"), Term("Summer")], is_core = True)
+
+    ################# MATH Electives (300 level) ###################
+    # --------------------------------------------------------------------------
+    test_major.add_course("MATH Elective 300+", 300, 4, [], [Term("Fall"), Term("Winter"), Term("Spring"), Term("Summer")], is_core = True)
+
+    return test_major
+
 def main():
     student_A = Student("student_A")
     student_A.add_degree(CIS_major)
